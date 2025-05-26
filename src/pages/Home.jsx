4827c28758805e7b2661f26de2f37c580432d988
Léon <leon.gallet@gmail.com>
2025-05-26 14:11:53 +0200

    Setup: Implement basic React app structure with header component and SASS styling

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import Header from "../components/atoms/Header/Header"
+
+export default function Home() {
+    return (
+        <>
+           <Header />
+           <h1>Home</h1>
+        </>
+    )
+}
