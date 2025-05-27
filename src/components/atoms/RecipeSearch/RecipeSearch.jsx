d73abe3aa99d64ea5bfba558b814a4b506637a12
Hicham Roldan <82762193+HDevv@users.noreply.github.com>
2025-05-27 15:12:17 +0200

    Component searchRecipes

--- src/components/atoms/RecipeSearch/RecipeSearch.jsx
+++ src/components/atoms/RecipeSearch/RecipeSearch.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
+import RecipeCard from "../../Molecules/RecipeCard/RecipeCard";
+import recipesData from "../../../data/recipes.json";
 import "./RecipeSearch.scss";
 
 export default function RecipeSearch() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const filteredRecipes = recipesData.filter((recipe) =>
+    `${recipe.title} ${recipe.description}`
+      .toLowerCase()
+      .includes(searchTerm.toLowerCase())
+  );
+
   return (
     <div className="recipe-search">
       <input
         type="text"
         className="recipe-search-input"
         placeholder="Rechercher une recette..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
       />
       <div className="recipe-search-results">
-        <p className="no-result">Aucune recette trouvée.</p>
+        {filteredRecipes.length > 0 ? (
+          filteredRecipes.map((recipe) => (
+            <RecipeCard
+              key={recipe.id}
+              title={recipe.title}
+              description={recipe.description}
+              image={recipe.image}
+            />
+          ))
+        ) : (
+          <p className="no-result">Aucune recette trouvée.</p>
+        )}
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,12 @@
-import RecipeCard from "@components/molecules/RecipeCard/RecipeCard";
+import RecipeSearch from "../components/Atoms/RecipeSearch/RecipeSearch";
+RecipeSearch;
 
 export default function Home() {
   return (
     <div style={{ padding: "2rem" }}>
+      {" "}
+      <RecipeSearch />
       <h1 style={{ color: "red" }}>Home</h1>
-      <RecipeCard
-        title="Spaghetti Carbonara"
-        image="https://placehold.co/400"
-        description="Un plat italien délicieux avec œuf, fromage et lardons."
-        />
     </div>
   );
 }
